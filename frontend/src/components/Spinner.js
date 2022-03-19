931ff270c0be9db9dfcab3577ad43ff732adff43
hsoubcontrolpanel <hsoubcontrolpanel@gmail.com>
2022-03-19 06:57:43 +0200

    update spinnner

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -1,18 +1,13 @@
-import React from "react" 
-import Loader from "react-loader-spinner" 
+import {Puff} from 'react-loader-spinner';
 
-export default class Spinner extends React.Component {
-    render() {
-        return (
-            <div className="d-flex justify-content-center">
-                <Loader
-                    type="Puff"
-                    color="#cc6600"
-                    height={100}
-                    width={100}
-                    timeout={3000} //3 secs
-                />
-            </div>
-        ) 
-    }
-}
+export const Spinner = () => (
+  <div className="d-flex justify-content-center ">
+    <Puff 
+        color='#cc6600'
+        height={100}
+        width={100}
+    />
+  </div>
+);
+
+export default Spinner;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,6 @@ import App from './App'
 import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider, split } from "@apollo/client" 
 import { getMainDefinition } from "@apollo/client/utilities" 
 import { setContext } from "apollo-link-context" 
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css" 
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
 
